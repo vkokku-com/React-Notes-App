42f5fdde0f23b9ff917f171f08718512db37885c
Tom Stones <tom@tomstones.com.au>
2015-07-13 14:09:43 +1000

    Finalised ES

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,53 +1,66 @@
-var React = require('react');
-var Router = require('react-router');
-var UserProfile = require('./Github/UserProfile');
-var Repos = require('./Github/Repos');
-var Notes = require('./Notes/Notes');
-var ReactFireMixin = require('reactfire');
-var FireBase = require('firebase');
-var helpers = require('../utils/helpers');
+import React from 'react';
+import UserProfile from './Github/UserProfile';
+import Repos from './Github/Repos';
+import Notes from './Notes/Notes';
+import FireBase from 'firebase';
+import helpers from '../utils/helpers';
+import Rebase from 're-base'
+//var Router = require('react-router');
+//var ReactFireMixin = require('reactfire');
 
-var Profile = React.createClass({
-    mixins: [
-        Router.State,
-        ReactFireMixin
-    ],
-    getInitialState: function () {
-        return {
+var base = Rebase.createClass('https://stones-react-notes.firebaseio.com/');
+
+class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
             bio: {name: ''},
             notes: [],
             repos: []
         }
-    },
+    }
 
-    init: function () {
+    init() {
+        this.ref = base.bindToState(this.router.getCurrentParams().username, {
+            context: this,
+            asArray: true,
+            state: 'notes'
+        });
 
-        var childRef = this.ref.child(this.getParams().username);
-        this.bindAsArray(childRef, 'notes');
-        helpers.getGithubInfo(this.getParams().username)
+        helpers.getGithubInfo(this.router.getCurrentParams().username)
             .then(function (dataObj) {
                 this.setState({
                     bio: dataObj.bio,
                     repos: dataObj.repos
                 });
             }.bind(this));
-    },
-    componentDidMount: function () {
-        this.ref = new FireBase('https://stones-react-notes.firebaseio.com/');
+    }
+
+    componentWillMount() {
+        this.router = this.context.router;
+    }
+
+    componentDidMount() {
         this.init();
-    },
-    componentWillUnmount: function () {
-        this.unbind('notes');
-    },
-    componentWillReceiveProps(){
-        this.unbind('notes');
+    }
+
+    componentWillUnmount() {
+        base.removeBinding(this.ref);
+    }
+
+    componentWillReceiveProps() {
+        base.removeBinding(this.ref);
         this.init();
-    },
-    handleAddNote: function (newNote) {
-        this.ref.child(this.getParams().username).set(this.state.notes.concat([newNote]));
-    },
-    render: function () {
-        var username = this.getParams().username;
+    }
+
+    handleAddNote(newNote) {
+        base.post(this.router.getCurrentParams().username, {
+            data: this.state.notes.concat([newNote])
+        })
+    }
+
+    render() {
+        var username = this.router.getCurrentParams().username;
         return (
             <div className="row">
                 <div className="col-md-4">
@@ -60,14 +73,18 @@ var Profile = React.createClass({
                     <Notes
                         username={username}
                         notes={this.state.notes}
-                        addNote={this.handleAddNote}
+                        addNote={this.handleAddNote.bind(this)}
                         />
                 </div>
             </div>
         );
     }
 
-});
 
+}
 
-module.exports = Profile;
+Profile.contextTypes = {
+    router: React.PropTypes.func.isRequired
+};
+
+export default Profile;
